db1137df31c723d79f70e7e9cf4fdfc74fc2d399
Faisalnk4045 <foysalnk9095@gmail.com>
2024-02-27 20:16:44 +0600

    add Hired component style

--- src/components/Hired/Coders.jsx
+++ src/components/Hired/Coders.jsx
@@ -2,12 +2,19 @@ import { Button } from "react-bootstrap";
 
 const Coders = ({ coder, handleRemove }) => {
     return (
-        <>
-            <div>
-                {coder.name} - {coder.salary}
+        <div className="d-flex border justify-content-between mt-1 p-2 shadow-sm">
+            <div className="d-flex align-items-center">
+                <img className="w-25" src={coder.img} alt="" />
+                <h6 className="ms-2 mb-0">{coder.name}</h6>
             </div>
-            <Button onClick={() => handleRemove(coder)}>remove</Button>
-        </>
+            <Button
+                variant="light"
+                className="text-danger bg-transparent border-0 fw-bold"
+                onClick={() => handleRemove(coder)}
+            >
+                <i className="fa-regular fa-trash-can"></i>
+            </Button>
+        </div>
     );
 };
 
--- src/components/Hired/Hired.jsx
+++ src/components/Hired/Hired.jsx
@@ -3,11 +3,21 @@ import Coders from "./Coders";
 const Hired = ({ hiredCoders, handleRemove, budgetRemaining }) => {
     const totalCost = 1000000 - budgetRemaining;
     return (
-        <div>
-            <p>hired: {hiredCoders.length}</p>
-            <p>cost: {totalCost}</p>
-            <p>remainingBudget: {budgetRemaining}</p>
+        <div className="border p-3 sticky-top">
+            <div className="d-flex justify-content-between border-bottom mt-2">
+                <p>Coders hired </p>
+                <b>{hiredCoders.length}</b>
+            </div>
+            <div className="d-flex justify-content-between border-bottom mt-2">
+                <p>Total cost </p>
+                <b>${totalCost}</b>
+            </div>
+            <div className="d-flex justify-content-between mt-2">
+                <p>Budget left </p>
+                <b>${budgetRemaining}</b>
+            </div>
             <div>
+                {hiredCoders.length ? <hr className="mt-0" /> : ""}
                 {hiredCoders.map((coder, idx) => (
                     <Coders
                         key={idx}
