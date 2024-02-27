3177f81e366a64223ba65e24870ae01c0b62efa2
Faisalnk4045 <foysalnk9095@gmail.com>
2024-02-27 17:20:26 +0600

    add Header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
+import "./Header.css";
+
 const Header = () => {
     return (
-        <header className="border border-2">
+        <header className="header my-3 p-3 rounded text-center shadow-sm">
             <h2>Make the ultimate programmers club</h2>
-            <p>
+            <p className="pt-2">
                 “Any fool can write code that a computer can understand. Good
                 programmers write code that humans can understand.” – Martin
                 Fowler
