89dc58c2505fe09babe512c8a707d3315b8356b0
Faisalnk4045 <foysalnk9095@gmail.com>
2024-02-27 10:11:28 +0600

    initialize project with vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Container } from "react-bootstrap";
+import "./App.css";
+
+function App() {
+    return (
+        <Container className="poppins-regular">
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque
+                rerum enim dolorem eius possimus dolorum. Ea blanditiis rem,
+                sed, necessitatibus commodi dignissimos quisquam error cumque
+                pariatur, corporis sapiente veniam iure!
+            </p>
+        </Container>
+    );
+}
+
+export default App;
